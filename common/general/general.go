47825fd4bba08ff32e35fbf48a0b6a2426680a4f
Sashwat K <Sashwat.K@ibm.com>
2024-04-11 09:10:10 +0530

    feat: add common package

---
+++ common/general/general.go
@@ -0,0 +1,90 @@
+package general
+
+import (
+	"bytes"
+	"encoding/base64"
+	"os"
+	"os/exec"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+// ExecCommand - function to run os commands
+func ExecCommand(name string, stdinInput string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+
+	// Check for standard input
+	if stdinInput != "" {
+		stdinPipe, err := cmd.StdinPipe()
+		if err != nil {
+			return "", err
+		}
+		defer stdinPipe.Close()
+
+		go func() {
+			defer stdinPipe.Close()
+			stdinPipe.Write([]byte(stdinInput))
+		}()
+	}
+
+	// Buffer to capture the output from the command.
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	// Run the command.
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	// Return the output from the command and nil for the error.
+	return out.String(), nil
+}
+
+// CreateTempFile - Function to create temp file
+func CreateTempFile(data string) (string, error) {
+
+	trimmedData := strings.TrimSpace(data)
+	tmpFile, err := os.CreateTemp("", "hpvs-")
+	if err != nil {
+		return "", err
+	}
+	defer tmpFile.Close()
+
+	// Write the data to the temp file.
+	_, err = tmpFile.WriteString(trimmedData)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the path to the temp file.
+	return tmpFile.Name(), nil
+}
+
+// EncodeToBase64 - function to encode string as base64
+func EncodeToBase64(input string) string {
+	return base64.StdEncoding.EncodeToString([]byte(input))
+}
+
+// MapToYaml - function to convert string map to YAML
+func MapToYaml(m map[string]interface{}) (string, error) {
+	// Marshal the map into a YAML string.
+	yamlBytes, err := yaml.Marshal(m)
+	if err != nil {
+		return "", err
+	}
+	return string(yamlBytes), nil
+}
+
+// KeyValueInjector - function to inject key value pair in YAML
+func KeyValueInjector(contract map[string]interface{}, key, value string) (string, error) {
+	contract[key] = value
+
+	modifiedYAMLBytes, err := yaml.Marshal(contract)
+	if err != nil {
+		return "", err
+	}
+
+	return string(modifiedYAMLBytes), nil
+}
---
+++ common/general/general_test.go
@@ -0,0 +1,112 @@
+package general
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	simpleContractPath = "../../samples/simple_contract.yaml"
+)
+
+func TestExecCommand(t *testing.T) {
+	_, err := ExecCommand("openssl", "", "version")
+
+	assert.NoError(t, err)
+}
+
+func TestExecCommandUserInput(t *testing.T) {
+	_, err := ExecCommand("openssl", "hello", "version")
+
+	assert.NoError(t, err)
+}
+
+func TestCreateTempFile(t *testing.T) {
+	text := "Testing"
+	tmpfile, err := CreateTempFile(text)
+
+	file, err1 := os.Open(tmpfile)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	defer file.Close()
+
+	content, err1 := io.ReadAll(file)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+
+	err1 = os.Remove(tmpfile)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+
+	assert.Equal(t, text, string(content))
+	assert.NoError(t, err)
+}
+
+func TestEncodeToBase64(t *testing.T) {
+	base64data := "c2FzaHdhdGs="
+	result := EncodeToBase64("sashwatk")
+
+	assert.Equal(t, result, base64data)
+}
+
+func TestMapToYaml(t *testing.T) {
+	var contractMap map[string]interface{}
+
+	file, err := os.Open(simpleContractPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = MapToYaml(contractMap["env"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.NoError(t, err)
+}
+
+func TestKeyValueInjector(t *testing.T) {
+	var contractMap map[string]interface{}
+	key := "envWorkloadSignature"
+	value := "testing123"
+
+	file, err := os.Open(simpleContractPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	finalContract, err := KeyValueInjector(contractMap, key, value)
+
+	assert.Contains(t, finalContract, fmt.Sprintf("%s: %s", key, value))
+	assert.NoError(t, err)
+}
