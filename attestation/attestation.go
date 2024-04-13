64aebfa7c05385f23978c5575164844a6224b931
Sashwat K <Sashwat.K@ibm.com>
2024-04-14 02:06:29 +0530

    feat: add support to decrypt attestation records - main fucntion

---
+++ attestation/attestation.go
@@ -0,0 +1,23 @@
+package attestation
+
+import (
+	attest "github.com/Sashwat-K/lib-hpcr/common/decrypt"
+	gen "github.com/Sashwat-K/lib-hpcr/common/general"
+)
+
+// GetAttestationRecords - function to get attestation records from encrypted data
+func GetAttestationRecords(data, privateKey string) (string, error) {
+	encodedEncryptedPassword, encodedEncryptedData := gen.GetEncryptPassWorkload(data)
+
+	password, err := attest.DecryptPassword(encodedEncryptedPassword, privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	attestationRecords, err := attest.DecryptWorkload(password, encodedEncryptedData)
+	if err != nil {
+		return "", err
+	}
+
+	return attestationRecords, nil
+}
---
+++ attestation/attestation_test.go
@@ -0,0 +1,48 @@
+package attestation
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	encryptedChecksumPath = "../samples/attestation/se-checksums.txt.enc"
+	privateKeyPath        = "../samples/attestation/private.pem"
+)
+
+// Testcase to check if TestGetAttestationRecords() retrives attestation records from encrypted data
+func TestGetAttestationRecords(t *testing.T) {
+	encChecksumPath, err := os.Open(encryptedChecksumPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer encChecksumPath.Close()
+
+	encChecksum, err := io.ReadAll(encChecksumPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	privateKey, err := os.Open(privateKeyPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer privateKey.Close()
+
+	privateKeyData, err := io.ReadAll(privateKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result, err := GetAttestationRecords(string(encChecksum), string(privateKeyData))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Contains(t, result, "baseimage")
+	assert.NoError(t, err)
+}
